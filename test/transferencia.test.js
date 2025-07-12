6b5b38e7c5d6e21c09102b7bfd9aba9e8932cdef
tgtorresqa <158749143+tgtorresqa@users.noreply.github.com>
2025-07-12 15:19:41 -0300

    Criei um helper de autenticação para obter o token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,19 +1,12 @@
 const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
+const { obterToken } = require('../helpers/autenticacao')
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-            })
-
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima', '123456')
             
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
@@ -29,15 +22,7 @@ describe('Transferencias', () => {
             expect(resposta.status).to.equal(201);
         })
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async () => {
-            const respostaLogin = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-            })
-
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima', '123456')
             
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
